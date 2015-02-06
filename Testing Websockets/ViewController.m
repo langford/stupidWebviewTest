ed8e5087b4fab8211becb646e1519126d211e904
Michael Langford <michael.langford@rowdylabs.com>
2015-02-06 17:21:10 -0500

    Real intial

--- Testing Websockets/ViewController.m
+++ Testing Websockets/ViewController.m
@@ -9,17 +9,25 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView* webView;
+@property (weak, nonatomic) IBOutlet UITextField* urlBar;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)goTapped:(id)sender
+{
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:self.urlBar.text]]];
+}
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
